3be7ca531577b2aecc5a2fab430864fec08b8ee2
patrikMarcok <patrik.marcok@student.tuke.sk>
2023-09-08 11:37:41 +0200

    page

--- src/GetData.js
+++ src/GetData.js
@@ -1,6 +1,6 @@
 // GetData.js
 import React, { useState, useEffect } from 'react';
-import { getFirestore, collection, getDocs, doc, deleteDoc } from 'firebase/firestore'; // Import Firestore functions
+import { getFirestore, collection, getDocs, deleteDoc, doc } from 'firebase/firestore'; // Import deleteDoc and doc
 import firebaseApp from './firebase';
 import './styles.css'; 
 
@@ -8,34 +8,29 @@ function GetData() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // Create a Firestore instance
-    const db = getFirestore(firebaseApp);
-
-    // Reference a Firestore collection
-    const dataCollection = collection(db, 'med'); // Replace with your collection name
-
-    // Fetch data from the collection
-    async function fetchData() {
-      const querySnapshot = await getDocs(dataCollection);
-      const newData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })); // Include document ID
-      setData(newData);
-    }
-
     fetchData();
   }, []);
 
-  // Function to handle item deletion
-  async function handleDelete(documentId) {
+  async function fetchData() {
     const db = getFirestore(firebaseApp);
-    const dataRef = doc(db, 'med', documentId);
+    const dataCollection = collection(db, 'med');
+
+    const querySnapshot = await getDocs(dataCollection);
+    const newData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    setData(newData);
+  }
+
+  // Add a function to handle delete
+  async function handleDelete(id) {
+    const db = getFirestore(firebaseApp);
+    const dataCollection = collection(db, 'med');
+    const dataDoc = doc(dataCollection, id);
 
     try {
-      await deleteDoc(dataRef);
-      // After successful deletion, you may want to update the state to remove the deleted item from the UI.
-      // You can fetch data again to refresh the list.
-      fetchData();
+      await deleteDoc(dataDoc);
+      fetchData(); // Refresh data after deletion
     } catch (error) {
-      console.error('Error deleting document:', error);
+      console.error('Error deleting document: ', error);
     }
   }
 
@@ -46,7 +41,8 @@ function GetData() {
         {data.map((item, index) => (
           <li key={item.id} className="list-item">
             {item.first_name} - {item.second_name} - {item.amount}
-            <button onClick={() => handleDelete(item.id)}>Delete</button>
+            {item.own_bottle && <span> - Own Bottle: Yes</span>}
+            <button onClick={() => handleDelete(item.id)}>Delete</button> {/* Add delete button */}
           </li>
         ))}
       </ul>
--- src/PostData.js
+++ src/PostData.js
@@ -10,18 +10,30 @@ class PostData extends Component {
       value1: '',
       value2: '',
       value3: '',
-      
+      ownBottle: false, // Initialize the checkbox state
     };
   }
 
-  
   handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
+    const { name, value, type, checked } = event.target;
+
+    // If the input is a checkbox, update the 'ownBottle' state
+    if (type === 'checkbox') {
+      this.setState({ [name]: checked });
+    } else {
+      this.setState({ [name]: value });
+    }
   };
 
   handleSubmit = async (event) => {
     event.preventDefault();
+
+    // Check if the "Amount" field contains only numbers
+    const amountPattern = /^[0-9]+$/;
+    if (!amountPattern.test(this.state.value3)) {
+      alert('Please enter a valid amount (numbers only)');
+      return; // Prevent submitting invalid data
+    }
 
     // Create a Firestore instance
     const db = getFirestore();
@@ -29,16 +41,17 @@ class PostData extends Component {
     // Reference the Firestore collection where you want to add the document
     const dataCollection = collection(db, 'med'); // Replace with your collection name
 
-    // Add a new document with the values from the form
+    // Add a new document with the values from the form, including the checkbox state
     try {
       await addDoc(dataCollection, {
         first_name: this.state.value1,
         amount: this.state.value3,
-        second_name: this.state.value2
+        second_name: this.state.value2,
+        own_bottle: this.state.ownBottle, // Include the checkbox value
       });
 
       // Clear the form after successfully adding the document
-      this.setState({ value1: '', value2: '', value3: ''});
+      this.setState({ value1: '', value2: '', value3: '', ownBottle: false });
     } catch (error) {
       console.error('Error adding document: ', error);
     }
@@ -46,43 +59,54 @@ class PostData extends Component {
 
   render() {
     return (
-        <div>
-            <h1>Post data</h1>
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    First name:
-                    <input
-                    type="text"
-                    name="value1"
-                    value={this.state.value1}
-                    onChange={this.handleChange}
-                    />
-                </label>
-                <br />
-                <label>
-                    Second name:
-                    <input
-                    type="text"
-                    name="value2"
-                    value={this.state.value2}
-                    onChange={this.handleChange}
-                    />
-                </label>
-                <br />
-                <label>
-                    Amount:
-                    <input
-                    type="text"
-                    name="value3"
-                    value={this.state.value3}
-                    onChange={this.handleChange}
-                    />
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-      );
+      <div>
+        <h1>Post data</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            First name:
+            <input
+              type="text"
+              name="value1"
+              value={this.state.value1}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Second name:
+            <input
+              type="text"
+              name="value2"
+              value={this.state.value2}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Amount:
+            <input
+              type="text"
+              name="value3"
+              value={this.state.value3}
+              onChange={this.handleChange}
+              pattern="[0-9]+" // Use the pattern attribute for numbers only
+            />
+          </label>
+          <br />
+          <label>
+            Own Bottle:
+            <input
+              type="checkbox"
+              name="ownBottle"
+              checked={this.state.ownBottle}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
